946d63c5dc980a2078b6d4dbfc923a0fe72ff5bc
zsund <zeon.h@gmail.com>
2021-05-27 16:53:42 +0900

    localhost:5000 index

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html',name="황지선")
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
