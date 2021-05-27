0908d4f0279c83e8c068bfeddd27eb58fe0915cb
zsund <zeon.h@gmail.com>
2021-05-27 17:35:16 +0900

    localhost:5000 instance

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
-    return render_template('home.html',name="황지선")
+    return render_template('home.html',name="HOME")
+
+@app.route('/about',methods=['GET','POST'])
+def about():
+    return render_template('about.html',name="ABOUT")
+
+@app.route('/articles',methods=['GET','POST'])
+def articles():
+    return render_template('articles.html',name="ARTICLES")
 
 if __name__=='__main__':
     app.run(debug=True)
